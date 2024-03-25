8f38690e6449fd6e00afc71d1bceb10da8966994
yosev <yosmisyael7308@gmail.com>
2024-03-25 17:04:19 +0700

    fix: fixed bugs on remove background feature

--- src/command/rmbg.ts
+++ src/command/rmbg.ts
@@ -1,21 +1,31 @@
 import {
     Attachment, AttachmentBuilder,
-    ContextMenuCommandInteraction,
+    ContextMenuCommandInteraction, SlashCommandAttachmentOption,
 } from "discord.js";
 import * as fs from "fs";
 import {CommandExt} from "../model/command";
 import axios, {AxiosResponse} from "axios";
 import * as path from "path";
 import * as buffer from "buffer";
-import {v4 as uuid} from "uuid";
 
-const getImageExtension = (imgUrl: string) => imgUrl.split("?")[0].split(".").pop()?.toLowerCase();
+function getAndValidateImageExtension(imageUrl: string): { isValid: boolean; extension: string } {
+    const imageExtension: string | undefined = imageUrl.split("?")[0].split(".").pop()?.toLowerCase();
+    const validExtension: string[] = ['jpg', 'jpeg', 'png', 'webp', 'gif'];
+    if (imageExtension && validExtension.includes(imageExtension)) {
+        return {
+            isValid: true,
+            extension: imageExtension
+        };
+    } else {
+        throw new Error("Unsupported image extension.")
+    }
+}
 
 async function convertImageToBlob(imagePath: string): Promise<Blob> {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, reject): void => {
         const stream = fs.createReadStream(imagePath);
         const chunks: buffer.Buffer[] = [];
-        stream.on("data", (chunk) => {
+        stream.on("data", (chunk: string | buffer.Buffer): void => {
             if (!Buffer.isBuffer(chunk)) {
                 chunks.push(Buffer.from(chunk))
             } else {
@@ -27,44 +37,58 @@ async function convertImageToBlob(imagePath: string): Promise<Blob> {
     })
 }
 
+async function downloadImage(imageUrl: string, outDir: string): Promise<string> {
+    const response: AxiosResponse = await axios.get(imageUrl, { responseType: "arraybuffer" });
+    if (response.status !== 200) throw new Error("[ERROR] Failed download image.");
+    const imageBuffer: Buffer = Buffer.from(response.data, "binary");
+    await fs.promises.writeFile(path.join(outDir), imageBuffer);
+    return outDir;
+}
+
+async function requestBackgroundRemoval(filePath: string) {
+    const formRequest: FormData = new FormData();
+    const imageBlob: Blob = await convertImageToBlob(filePath);
+    formRequest.append("image", imageBlob);
+    const response: AxiosResponse = await axios.post("http://127.0.0.1:5100", formRequest, {
+        headers: {
+            "Content-Type": "multipart/form-data"
+        },
+        responseType: "stream"
+    });
+
+    if (response.status !== 200) {
+        throw new Error("[ERROR] Failed to get result from RMBG API.")
+    }
+
+    return response.data;
+}
+
 async function handleImage(image: Attachment): Promise<string | undefined> {
     const imageUrl: string = image.url;
-    const validExtension: string[] = ['jpg', 'jpeg', 'png'];
-    const imageExtension: string | undefined = getImageExtension(imageUrl);
+
+    // get and validate image extension from discord
+    const { extension }: { extension: string } = getAndValidateImageExtension(imageUrl);
+
     const imageDir: string = path.join(__dirname, "../../image");
 
-    if (imageExtension && !validExtension.includes(imageExtension)) throw new Error("Unsupported image extension.");
-
     try {
-        const response: AxiosResponse = await axios.get(imageUrl, { responseType: "arraybuffer"});
-        const imgBuffer: Buffer = Buffer.from(response.data, "binary");
-        await fs.promises.writeFile(path.join(imageDir, `image.${imageExtension}`), imgBuffer);
-        const formRequest: FormData = new FormData();
-        const imageBlob: Blob = await convertImageToBlob(path.join(imageDir, `image.${imageExtension}`));
-        formRequest.append("image", imageBlob);
-        const rmbgResponse: AxiosResponse = await axios.post("http://127.0.0.1:5100", formRequest, {
-            headers: {
-                "Content-Type": "multipart/form-data"
-            },
-            responseType: "stream"
-        });
-        if (rmbgResponse.status !== 200) {
-            throw new Error("Failed get result from rmbg api");
-        }
-        const filename = uuid() + ".png";
-        await rmbgResponse.data.pipe(fs.createWriteStream(path.join(imageDir, `output/${filename}`)));
-        console.log("downloaded")
+        const downloadedImagePath: string = await downloadImage(imageUrl, path.join(imageDir, `image.${extension}`));
+        const response = await requestBackgroundRemoval(downloadedImagePath);
+        const filename: string = "removed_background.png";
+        await fs.promises.writeFile(path.join(imageDir, `output/${filename}`), response);
         return path.join(imageDir, `output/${filename}`);
     } catch (e: unknown) {
-        console.log(`Error occurred when saving image: ${e as string}`);
+        if (e instanceof Error) {
+            console.log(`[ERROR] ${e.message}`);
+        }
     }
 }
 
 export default {
     data: new CommandExt()
         .setName("rmbg")
-        .setDescription("Remove background from image.")
-        .addAttachmentOption((options) => options
+        .setDescription("Remove background from image. Supported image extensions are .jpg, .png, .webp, and .gif.")
+        .addAttachmentOption((options: SlashCommandAttachmentOption) => options
             .setRequired(true)
             .setName("image")
             .setDescription("image to remove background")),
@@ -73,21 +97,22 @@ export default {
         const attachment: Attachment = interaction.options.getAttachment("image") as Attachment;
         try {
             const result: string | undefined = await handleImage(attachment);
-
             if (!result) {
                 await interaction.editReply({ content: "Sorry, an error occurred during background removal." });
                 return;
             }
-            const replyAttachment: AttachmentBuilder = new AttachmentBuilder(path.join(__dirname, "../../image/output/background_removed.png"));
+            const replyAttachment: AttachmentBuilder = new AttachmentBuilder(result);
+
             await interaction.followUp({
                 content: "Here is your removed background image.",
                 files: [replyAttachment],
             });
 
-        } catch (error) {
-            console.error("Error:", error);
+        } catch (error: unknown) {
+            if (error instanceof Error) {
+                console.error(`[Error] ${error.message}`);
+            }
             await interaction.editReply({ content: "An error occurred. Please try again later." });
         }
-
     }
 };
